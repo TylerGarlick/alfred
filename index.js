b82d5871b7463e0ef9cf33eff3ba48a62ced3e62
Tyler Garlick <tjgarlick@gmail.com>
2014-06-12 20:15:11 -0600

    fixes

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 var Hapi = require('hapi');
 
 var port = process.env.PORT || 3000;
-var server = new Hapi.Server(port, { files: { relativeTo: __dirname } });
+var server = new Hapi.Server(port, { cors: true, files: { relativeTo: __dirname } });
 
 server.route([
   { method: 'GET', path: '/{path*}', handler: { directory: { path: './public', listing: true } } }
 ]);
 
-server.start();
+server.pack.register([
+  { plugin: require('lout') },
+  { plugin: require('good'),
+    options: {
+      subscribers: {
+        'console': ['ops', 'request', 'log', 'error'],
+        'http://localhost/logs': ['log'],
+        '/tmp/logs/': ['request', 'log']
+      }
+    }
+  }
+], function () {
+  server.start(function () {
+    console.log('Server started at: ' + server.info.uri);
+  });
+});
